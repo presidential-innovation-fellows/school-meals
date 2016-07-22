f151557fc7d72044c2ddc08db6c1b50c8111e275
Ross Dakin <rossdakin@gmail.com>
2016-07-22 08:28:53 -0400

    Create language switcher
    Doesn't do anything other than indicate language in search query

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { observer } from 'mobx-react'
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
 @observer
 class Navigation extends Component {
@@ -9,6 +9,28 @@ class Navigation extends Component {
   }
 
   render () {
+    let lang = 'en'
+
+    if (window.location.search.split('?')[1]) {
+      let pairs = window.location.search.split('?')[1].split('=')
+      for (var i = 0; i < pairs.length; i += 2) {
+        if (pairs[i] === 'lang') {
+          lang = pairs[i + 1]
+        }
+      }
+    }
+
+    const map = {
+      en: 'English',
+      zh: '中文',
+      fr: 'Français',
+      pa: 'ਪੰਜਾਬੀ',
+      ru: 'Русский',
+      es: 'Español',
+      tl: 'Wikang Tagalog',
+      vi: 'Tiếng Việt'
+    }
+
     return (
       <Navbar fixedTop>
         <Navbar.Header>
@@ -19,7 +41,16 @@ class Navigation extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem eventKey={1} href="">English</NavItem>
+            <NavDropdown eventKey="1" title={map[lang]} id="nav-dropdown">
+              <MenuItem eventKey="1.1" lang="en" href="/?lang=en">English</MenuItem>
+              <MenuItem eventKey="1.2" lang="zh" href="/?lang=zh">我说中文</MenuItem>
+              <MenuItem eventKey="1.3" lang="fr" href="/?lang=fr">Je parle français</MenuItem>
+              <MenuItem eventKey="1.4" lang="pa" href="/?lang=pa">ਇ ਸ੍ਪੇਆਕ ਪੰਜਾਬੀ</MenuItem>
+              <MenuItem eventKey="1.5" lang="ru" href="/?lang=ru">Я говoрю по-русски</MenuItem>
+              <MenuItem eventKey="1.6" lang="es" href="/?lang=es">Yo hablo español</MenuItem>
+              <MenuItem eventKey="1.7" lang="tl" href="/?lang=tl">Marunong po akong magsalita ng Tagalog</MenuItem>
+              <MenuItem eventKey="1.8" lang="vi" href="/?lang=vi">Tôi nói tiếng Việt</MenuItem>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
