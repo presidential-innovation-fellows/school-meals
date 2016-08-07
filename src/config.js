aa94b4f703f481082c44c7ec5d184aea97ac85fa
Ross Dakin <rossdakin@gmail.com>
2016-08-07 14:48:46 -0400

    Use dummy data for school district info

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 export const organization = {
-  name: 'Baltimore School District',
+  name: 'Your School District ',
   contact: {
-    address: '200 E. North Avenue, Baltimore, MD 21202',
-    phone: '443-984-2000',
-    email: 'nutrition@bcps.k12.md.us'
+    address: '100 Main Street, Anytown, USA 12345',
+    phone: '555-123-4567',
+    email: 'sfa@state.us'
   },
   paperApplication: {
-    url: 'http://www.baltimorecityschools.org/Page/24473',
-    address: '200 E. North Avenue, Baltimore, MD 21202',
-    phone: '443-984-2000',
-    email: 'nutrition@bcps.k12.md.us'
+    url: 'http://www.fns.usda.gov/sites/default/files/cn/SP34-2016a2.pdf',
+    address: '100 Main Street, Anytown, USA 12345',
+    phone: '555-123-4567',
+    email: 'sfa@state.us'
   }
 }
 
@@ -19,11 +19,11 @@ export const assistancePrograms = ['CalFresh',
 
 export const localPrograms = {
   homelessness: {
-    contact: 'Julie Marie (888-555-1212)'
+    contact: 'Cersei Lannister (888-555-1212)'
   },
   snap: {
     name: 'CalFresh',
-    localContact: 'Robert Noyes (888-555-1212)',
+    localContact: 'Jon Snow (888-555-1212)',
     stateHotline: '888-555-1212'
   },
   tanf: {
