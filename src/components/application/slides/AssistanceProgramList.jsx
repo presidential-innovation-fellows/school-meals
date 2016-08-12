b4a1555269a303f15cf29e896eb46d9ce73f6108
Ross Dakin <rossdakin@gmail.com>
2016-08-12 00:50:25 -0400

    Make assistance program form larger

--- src/components/application/slides/AssistanceProgramList.jsx
+++ src/components/application/slides/AssistanceProgramList.jsx
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react'
+import Form from '../Form'
+import Fieldset from '../Fieldset'
 import InputField from '../InputField'
 import { observer } from 'mobx-react'
 import { AssistancePrograms as Store } from '../../../stores/ApplicationData'
@@ -9,19 +11,21 @@ class AssistanceProgramList extends Component {
     const { assistancePrograms } = this.props
 
     return(
-      <div>
-        <p>Please enter your case number(s):</p>
+      <Form large>
+        <Fieldset legend="Case numbers">
+          <p>Please enter your case number(s):</p>
 
-        {assistancePrograms.map(program =>
-          <InputField
-              key={program.id}
-              name="caseNumber"
-              label={program.name}
-              object={program}
-              placeholder="Case number"
-          />
-        )}
-      </div>
+          {assistancePrograms.map(program =>
+            <InputField
+                key={program.id}
+                name="caseNumber"
+                label={program.name}
+                object={program}
+                placeholder="Case number"
+            />
+           )}
+        </Fieldset>
+      </Form>
     )
   }
 }
