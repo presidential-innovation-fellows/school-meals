4d69222a75ada0a447bd89b2a6132531c562b6d6
Ross Dakin <rossdakin@gmail.com>
2016-08-12 00:36:18 -0400

    Tweak demographics layout

--- src/components/application/slides/DemographicsForm.jsx
+++ src/components/application/slides/DemographicsForm.jsx
@@ -12,39 +12,41 @@ class Demographics extends Component {
     const { student } = this.props
 
     return (
-      <Form large>
-        <div className="well">
-          <h2>{informalName(student)}</h2>
-          <label>Ethnicity</label>
-          <BooleanRadio name="isHispanicLatino"
-                        object={student.demographics}
-                        trueLabel="Hispanic or Latino"
-                        falseLabel="Non Hispanic or Latino" />
+      <div className="demographics-container">
+        <Form large>
+          <div className="well">
+            <h2>{informalName(student)}</h2>
+            <label>Ethnicity</label>
+            <BooleanRadio name="isHispanicLatino"
+                          object={student.demographics}
+                          trueLabel="Hispanic or Latino"
+                          falseLabel="Non Hispanic or Latino" />
 
-          <label>Race</label>
-          <Checkboxes legend="Races">
-            <Checkbox object={student.demographics} name="isNativeAmerican">
-              American Indian or Alaskan Native
-            </Checkbox>
-            <Checkbox object={student.demographics} name="isAsian">
-              Asian
-            </Checkbox>
-            <Checkbox object={student.demographics} name="isBlack">
-              Black or African American
-            </Checkbox>
-            <Checkbox object={student.demographics} name="isPacificIslander">
-              Native Hawaiian or Other Pacific Islander
-            </Checkbox>
-            <Checkbox object={student.demographics} name="isWhite">
-              White
-            </Checkbox>
-          </Checkboxes>
+            <label>Race</label>
+            <Checkboxes legend="Races">
+              <Checkbox object={student.demographics} name="isNativeAmerican">
+                American Indian or Alaskan Native
+              </Checkbox>
+              <Checkbox object={student.demographics} name="isAsian">
+                Asian
+              </Checkbox>
+              <Checkbox object={student.demographics} name="isBlack">
+                Black or African American
+              </Checkbox>
+              <Checkbox object={student.demographics} name="isPacificIslander">
+                Native Hawaiian or Other Pacific Islander
+              </Checkbox>
+              <Checkbox object={student.demographics} name="isWhite">
+                White
+              </Checkbox>
+            </Checkboxes>
 
-          <div>
-            <button className="usa-button-gray" type="reset">Clear</button>
+            <div>
+              <button className="usa-button-gray" type="reset">Clear</button>
+            </div>
           </div>
-        </div>
-      </Form>
+        </Form>
+      </div>
     )
   }
 }
