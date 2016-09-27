dd646d3b578311a3ba908b3356008df404426c73
TimOPI <Timothy.Wepplo@fns.usda.gov>
2016-09-27 18:50:24 -0400

    Create tooltip doc
    To house all tooltip text

---
+++ src/components/Tooltiptext.jsx
@@ -0,0 +1,11 @@
+﻿export const tooltiptext = {
+  letter: 'Check that the letter includes all the students in the household, because they are eligible for free meals.  If not, contact the school to correct the mistake.',
+  threethingsfirst: 'If you are unsure if you qualify, fill out an application and officials at',
+  threethingssecond: 'will determine if you are eligible.',
+  status: 'The non-cash benefits received through the school meal programs are not subject to public charge consideration.  You will not be deported, denied entry to the country, or denied permanent status because you apply for or receive school meal benefits.',
+  snap: 'The Supplemental Nutrition Assistance Program (SNAP) is a program that offers nutrition assistance to millions of eligible, low-income individuals and families.',
+  tanf: 'The Temporary Assistance for Needy Families (TANF) program is designed to help needy familities achieve self-sufficiency by providing cash assistance, as well as other supports and services.',
+  fdpir: 'The Food Distribution Program on Indian Reservations (FDPIR) is a Federal program that provides USDA foods to low-icome households, including the elderly, living on Indian reservations, and to Native American families residing in designated areas near reservations and in the State of Oklahoma.',
+  household: 'A household includes grandparents or other extended family members that are living with you.  It also includes people that are away on a temporary basis, like kids that are away at college.  It includes people regardless of age or whether they earn or receive income.'
+}
+
