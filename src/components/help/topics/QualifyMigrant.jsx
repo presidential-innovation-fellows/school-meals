63f28c7a282a5db51d66e11e6ef6a3569f51d2a0
TimOPI <Timothy.Wepplo@fns.usda.gov>
2016-10-21 17:38:56 -0400

    Create help text where formatting is not simple
    (cherry picked from commit 24cdde91ca823c4c2f69cf1120032aff4e459402)

---
+++ src/components/help/topics/QualifyMigrant.jsx
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react'
+import Topic from '../Topic'
+import { organization } from '../../../../config'
+
+//F20
+export default class QualifyMigrant extends Component {
+  render() {
+    return (
+      <Topic title="How do I know if my children qualify as migrant? ">
+        <p>
+          Your children may qualify as a migrant if you have moved your household into a different school district within the last three years to gain or look for temporary/seasonal work in agriculture or fishing.
+        </p>
+        <p>
+        	If you believe children in your household meet one or more of these descriptions and you haven’t been told your children will get free meals, please contact {organization.name}.
+        </p>
+      </Topic>
+    )
+  }
+}
