f84f4d12b4c33f92bd3802f1003af5c12ae6e8b0
Ambit Developer <ambit.developer@gmail.com>
2016-10-30 11:24:14 -0400

    Localize Welcome slide

--- src/components/application/slides/Welcome.jsx
+++ src/components/application/slides/Welcome.jsx
@@ -1,28 +1,61 @@
 ﻿import React from 'react'
+import FormattedMessage from '../FormattedMessage'
 import Slide from '../Slide'
 import { organization } from '../../../config'
+import { observer } from 'mobx-react'
 
+@observer
 class Welcome extends React.Component {
   render() {
+    // pulled out to be localized
+    const headerText = 'Welcome!'
+    const nextButtonText = 'Get Started'
+
     return (
-      <Slide header="Welcome!" id="welcome" showBack={false} nextText="Get Started" beginsSection>
+      <Slide header={headerText} id="welcome" showBack={false} nextText={nextButtonText} beginsSection>
+
         <p className="usa-font-lead">
-          This is the {organization.name} electronic application for free and
-          reduced priced meals for
-          the <abbr title="United States Department of Agriculture">USDA</abbr>&rsquo;s National School Lunch &amp; School Breakfast Programs.
+          <FormattedMessage
+              id="app.slides.welcome.intro"
+              description="Introductory paragraph."
+              defaultMessage="This is the {organizationName} electronic application for free and reduced priced meals for the {usda}&rsquo;s National School Lunch &amp; School Breakfast Programs."
+              values={{
+                organizationName: organization.name,
+                usda: <abbr title="United States Department of Agriculture">USDA</abbr>
+              }}
+          />
         </p>
+
         <p>
-          This interactive application will guide you through the application
-          process. When
-          you are ready to apply, click the ‘Get Started’ button below to
-          begin.
+          <FormattedMessage
+              id="app.slides.welcome.onlineInstructions"
+              description="Instructions on how to apply online."
+              defaultMessage="This interactive application will guide you through the application process. When you are ready to apply, click the ‘{buttonText}’ button below to begin."
+              values={{
+                buttonText: nextButtonText
+              }}
+          />
         </p>
+
         <p>
-          If you would like to apply using
-          the <a href={organization.paperApplication.url} target="_blank">paper application</a>,
-          you can print and complete it and then return it to us at 
-          {organization.paperApplication.address}.
+          <FormattedMessage
+              id="app.slides.welcome.paperInstructions"
+              description="Instructions on how to apply with a paper application."
+              defaultMessage="If you would like to apply using the {link}, you can print and complete it and then return it to us at {address}."
+              values={{
+                address: organization.paperApplication.address,
+                link:
+                     <a href={organization.paperApplication.url} target="_blank">
+                       <FormattedMessage
+                         id="app.slides.welcome.paperApplication"
+                         description="Phrase"
+                         defaultMessage="paper application"
+                       />
+                     </a>
+              }}
+          />
         </p>
+
       </Slide>
     )
   }
