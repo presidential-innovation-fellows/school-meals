330d697f2315e0c8b2f24e93d24e086088b7c2af
Ambit Developer <ambit.developer@gmail.com>
2016-11-06 10:45:43 -0700

    Localize and translate remaining Before You Begin tooltips

--- src/components/Tooltiptext.jsx
+++ src/components/Tooltiptext.jsx
@@ -21,9 +21,21 @@ export const tooltiptext = {
     description="Tooltip text about this program not resulting in public charges."
     defaultMessage="The non-cash benefits received through the school meal programs are not subject to public charge consideration. You will not be deported, denied entry to the country, or denied permanent status because you apply for or receive school meal benefits."
   />,
-  snap: 'The Supplemental Nutrition Assistance Program (SNAP) is a program that offers nutrition assistance to millions of eligible, low-income individuals and families.',
-  tanf: 'The Temporary Assistance for Needy Families (TANF) program is designed to help needy familities achieve self-sufficiency by providing cash assistance, as well as other supports and services.',
-  fdpir: 'The Food Distribution Program on Indian Reservations (FDPIR) is a Federal program that provides USDA foods to low-icome households, including the elderly, living on Indian reservations, and to Native American families residing in designated areas near reservations and in the State of Oklahoma.',
+  snap: <FormattedMessage
+    id="tooltip.snap"
+    description="Tooltip text explaining SNAP."
+    defaultMessage="The Supplemental Nutrition Assistance Program (SNAP) is a program that offers nutrition assistance to millions of eligible, low-income individuals and families."
+  />,
+  tanf: <FormattedMessage
+    id="tooltip.tanf"
+    description="Tooltip text explaining TANF."
+    defaultMessage="The Temporary Assistance for Needy Families (TANF) program is designed to help needy familities achieve self-sufficiency by providing cash assistance, as well as other supports and services."
+  />,
+  fdpir: <FormattedMessage
+    id="tooltip.fdpir"
+    description="Tooltip text explaining FDPIR."
+    defaultMessage="The Food Distribution Program on Indian Reservations (FDPIR) is a Federal program that provides USDA foods to low-icome households, including the elderly, living on Indian reservations, and to Native American families residing in designated areas near reservations and in the State of Oklahoma."
+  />,
   household: 'A household includes grandparents or other extended family members that are living with you.  It also includes people that are away on a temporary basis, like kids that are away at college. It includes people regardless of age or whether they earn or receive income.',
   foster: 'Foster child(ren) are eligible for free school meal benefits.  This only applies to children who are formally placed by the State welfare agency or court in a caretaker household.',
   householdreminder: 'Remember, for the purposes of applying for school meal benefits, a household is defined as a group of people, related or unrelated, that usually live together and share income and expenses.',
