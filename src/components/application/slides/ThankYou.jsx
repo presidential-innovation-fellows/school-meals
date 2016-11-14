d5de528a3396674b769e2eba00701fbe8e9aa449
rcoop017 <rcooper@theambitgroup.com>
2016-11-14 17:43:08 -0500

    Localized Thank You slide
    Localized ThankYou.jsx

--- src/components/application/slides/ThankYou.jsx
+++ src/components/application/slides/ThankYou.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Slide from '../Slide'
 import { observer } from 'mobx-react'
 import { thankYou } from '../../../config'
+import FormattedMessage from '../FormattedMessage'
 
 @observer
 class ThankYou extends Component {
@@ -9,9 +10,21 @@ class ThankYou extends Component {
     return (
       <Slide header="Thank you for applying for school meal benefits!" id="thank-you"
              showBack={false} nextText="View Final Data Set" beginsSection>
-        <p>Your application has been submitted.</p>
+        <p>
+        <FormattedMessage
+              id="app.slides.thankYou.submitted"
+              description="application has been submitted"
+              defaultMessage="Your application has been submitted."
+        />
+        </p>
         <p>{ thankYou }</p>
-        <p>[CAUTION! NO DATA HAS BEEN SAVED. THIS IS NOT A REAL APPLICATION FOR SCHOOL MEAL BENEFITS. THIS IS A MODEL APPLICATION DEVELOPED BY USDA TO DEMONSTRATE THE POTENTIAL FUNCTIONALITY OF A SCHOOL DISTRICT'S APPLICATION. CONTACT YOUR CHILD'S SCHOOL TO FIND OUT WHERE YOU CAN ACCESS THEIR APPLICATION FOR SCHOOL MEAL BENEFITS.]</p>
+        <p>
+        <FormattedMessage
+              id="app.slides.thankYou.caution"
+              description="Warning message"
+              defaultMessage="[CAUTION! NO DATA HAS BEEN SAVED. THIS IS NOT A REAL APPLICATION FOR SCHOOL MEAL BENEFITS. THIS IS A MODEL APPLICATION DEVELOPED BY USDA TO DEMONSTRATE THE POTENTIAL FUNCTIONALITY OF A SCHOOL DISTRICT'S APPLICATION. CONTACT YOUR CHILD'S SCHOOL TO FIND OUT WHERE YOU CAN ACCESS THEIR APPLICATION FOR SCHOOL MEAL BENEFITS.]"
+        />
+        </p>
       </Slide>
     )
   }
