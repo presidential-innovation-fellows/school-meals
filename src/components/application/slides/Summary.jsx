900e692303b7a96dddf540c66816c6f59e9a286e
rcoop017 <rcooper@theambitgroup.com>
2016-11-14 17:42:13 -0500

    Localized Summary slide
    Localized Summary.jsx

--- src/components/application/slides/Summary.jsx
+++ src/components/application/slides/Summary.jsx
@@ -34,25 +34,56 @@ class Summary extends Component {
              nextText="Submit"
              nextDisabled={!this.isValid}
              id="summary">
-        <p className="usa-font-lead">Awesome, you finished!</p>
-        <p>Here is a summary of the information you provided in the application. We encourage you to save or print this screen for your records. If everything looks good, click the "Submit" button at the bottom of the page.</p>
-
+        <p className="usa-font-lead">
+        <FormattedMessage
+          id="app.slides.summary.finished"
+          description="Awesome, you're finished!"
+          defaultMessage="Awesome, you finished!"
+        />
+        </p>
+        <p>
+        <FormattedMessage
+          id="app.slides.summary.summaryInfo"
+          description="Summary Info is below"
+          defaultMessage="Here is a summary of the information you provided in the application. We encourage you to save or print this screen for your records. If everything looks good, click the 'Submit' button at the bottom of the page."
+        />
+        </p>
         <div className="well">
           <SummaryPersonCollection collection={students} id="students">
-            Students attending {organization.name}
+          <FormattedMessage
+            id="app.slides.summary.studentsAttending"
+            description="student info"
+            defaultMessage="Students attending {organizationName}"
+            values={{
+              organizationName: organization.name
+            }}
+          />
           </SummaryPersonCollection>
 
           <SummaryPersonCollection collection={otherChildren} id="other-children">
-            Other children
+          <FormattedMessage
+            id="app.slides.summary.otherChildren"
+            description="Other children"
+            defaultMessage="Other children"
+          />
           </SummaryPersonCollection>
 
           <SummaryPersonCollection collection={adults} id={adultsId}>
-            Adults
+          <FormattedMessage
+            id="app.slides.summary.adults"
+            description="Adults"
+            defaultMessage="Adults"
+          />
           </SummaryPersonCollection>
 
           <div>
             <SummaryLabel id="assistance-programs">
-              Assistance program case numbers
+            <FormattedMessage
+              id="app.slides.summary.caseNumbers"
+              description="Assistance program case numbers"
+              defaultMessage="Assistance program case numbers"
+            />
+              
             </SummaryLabel>
 
             <ul>
@@ -71,7 +102,13 @@ class Summary extends Component {
 
           {applicationData.showHousehold &&
            <div>
-             <SummaryLabel>Total household income</SummaryLabel>
+             <SummaryLabel>
+             <FormattedMessage
+                id="app.slides.summary.totalIncome"
+                description="Total household income"
+                defaultMessage="Total household income"
+              />
+              </SummaryLabel>
              <Tooltipcomp text={tooltiptext.totalHouseholdIncome}>
                ${
                  numberFormat(
@@ -80,12 +117,22 @@ class Summary extends Component {
                  )
                 }
                {' '}
-               per month
+               <FormattedMessage
+                id="app.slides.summary.perMonth"
+                description=" per month"
+                defaultMessage=" per month"
+              />
              </Tooltipcomp>
            </div>
           }
 
-          <SummaryLabel id="contact">Contact information</SummaryLabel>
+          <SummaryLabel id="contact">
+          <FormattedMessage
+                id="app.slides.summary.contactInfo"
+                description="Contact information"
+                defaultMessage="Contact information"
+          />
+              </SummaryLabel>
           <p>
             { fullName(applicationData.attestor) }
             {!!contact.address1 &&
@@ -128,41 +175,67 @@ class Summary extends Component {
           <Checkbox name="certifiedCorrect" object={applicationData}>
             { applicationData.showHousehold ?
               <strong>
-                I certify* that
-                {' '}
-                <span className="usa-label-big">
-                  {applicationData.totalHouseholdMembers} people
-                </span>
-                are in my household and that our household income is about
-                {' '}
-                <span className="usa-label-big">
+              <FormattedMessage
+                id="app.slides.summary.certification"
+                description="Certification statement"
+                defaultMessage="I certify* that {totalHouseholdMembers} are in my household and that our household income is about {totalHouseholdIncome}"
+                values={{
+                totalHouseholdMembers: <span className="usa-label-big">
+                  {applicationData.totalHouseholdMembers} 
+                  <FormattedMessage
+                    id="app.slides.summary.people"
+                    description="people"
+                    defaultMessage=" people"
+                  />
+                </span>,
+                totalHouseholdIncome: <span className="usa-label-big">
                   ${numberFormat(applicationData.totalMonthlyHouseholdIncome)}
                   {' '}
-                  per month
+                  <FormattedMessage
+                    id="app.slides.summary.perMonth"
+                    description="per month"
+                    defaultMessage=" per month"
+                  />
                 </span>
+                }}
+                />
               </strong>
               : (assistancePrograms.length ?
                  <strong>
-                   I certify* that my household participates in
-                   {' '}
-                   {toSentenceSerialArray(assistancePrograms.map(program => {
+                 <FormattedMessage
+                    id="app.slides.summary.certificationPrograms"
+                    description="Certification statement for programs"
+                    defaultMessage="I certify* that my household participates in {program}"
+                    values={{
+                    program: toSentenceSerialArray(assistancePrograms.map(program => {
                       return (
                         <span className="usa-label-big" key={program.id}>
                           {program.accronym}
                         </span>
                       )
-                    }))}
+                        }))
+                    }}
+                />
                  </strong>
                  :
                  <strong>
-                   I certify* that the information on this page is correct to
-                   the best of my knowledge.
+                 <FormattedMessage
+                    id="app.slides.summary.infoCorrect"
+                    description="acknowledge info is correct"
+                    defaultMessage="I certify* that the information on this page is correct to the best of my knowledge."
+                 />
                  </strong>
               )
             }
           </Checkbox>
         </Checkboxes>
-        <p><small><em>*I understand that this information is given in connection with the receipt of Federal funds, and that school officials may verify (check) the information. I am aware that if I purposely give false information, my children may lose meal benefits. Deliberate misrepresentation of information may subject applicants to prosecution under applicable State and Federal law.</em></small></p>
+        <p><small><em>
+        <FormattedMessage
+          id="app.slides.summary.finePrint"
+          description="fine print, if info is wrong, it is fraud."
+          defaultMessage="*I understand that this information is given in connection with the receipt of Federal funds, and that school officials may verify (check) the information. I am aware that if I purposely give false information, my children may lose meal benefits. Deliberate misrepresentation of information may subject applicants to prosecution under applicable State and Federal law."
+        />
+        </em></small></p>
       </Slide>
     )
   }
