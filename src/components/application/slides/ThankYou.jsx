649a4e587012203b26f6f40e2927467ed7747bf7
Ambit Developer <ambit.developer@gmail.com>
2016-11-18 10:19:27 -0500

    Localization of Thank You text

--- src/components/application/slides/ThankYou.jsx
+++ src/components/application/slides/ThankYou.jsx
@@ -19,19 +19,25 @@ class ThankYou extends Component {
       <Slide header="Thank you for applying for school meal benefits!" id="thank-you"
              showBack={false} showNext={false} beginsSection>
         <p>
-        <FormattedMessage
+          <FormattedMessage
               id="app.slides.thankYou.submitted"
               description="application has been submitted"
               defaultMessage="Your application has been submitted."
-        />
+          />
         </p>
-        <p>{ thankYou }</p>
         <p>
-        <FormattedMessage
+          <FormattedMessage
+              id="app.slides.thankYou.nextSteps"
+              description="Indication of what happens next."
+              defaultMessage="You will hear from us soon with your certification decision!"
+          />
+        </p>
+        <p>
+          <FormattedMessage
               id="app.slides.thankYou.caution"
               description="Warning message"
               defaultMessage="[CAUTION! NO DATA HAS BEEN SAVED. THIS IS NOT A REAL APPLICATION FOR SCHOOL MEAL BENEFITS. THIS IS A MODEL APPLICATION DEVELOPED BY USDA TO DEMONSTRATE THE POTENTIAL FUNCTIONALITY OF A SCHOOL DISTRICT'S APPLICATION. CONTACT YOUR CHILD'S SCHOOL TO FIND OUT WHERE YOU CAN ACCESS THEIR APPLICATION FOR SCHOOL MEAL BENEFITS.]"
-        />
+          />
         </p>
         <Button onClick={this.downloadData}>
           <FormattedMessage
