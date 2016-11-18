3d647ffa0e8624a6bc14ef72fdee227b83e0c56c
Ambit Developer <ambit.developer@gmail.com>
2016-11-18 10:17:24 -0500

    Allow data to be downloaded in Thank You screen

--- src/components/application/slides/ThankYou.jsx
+++ src/components/application/slides/ThankYou.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import Slide from '../Slide'
+import Button from '../Button'
+import download from 'downloadjs'
 import { observer } from 'mobx-react'
 import { thankYou } from '../../../config'
-import {FormattedMessage} from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 
 @observer
 class ThankYou extends Component {
+  downloadData() {
+    var stringData = JSON.stringify(applicationData, undefined, 2)
+
+    download(stringData, 'nslp-data.json', 'text/plain');
+  }
+
   render() {
     return (
       <Slide header="Thank you for applying for school meal benefits!" id="thank-you"
-             showBack={false} nextText="View Final Data Set" beginsSection>
+             showBack={false} showNext={false} beginsSection>
         <p>
         <FormattedMessage
               id="app.slides.thankYou.submitted"
@@ -25,6 +33,13 @@ class ThankYou extends Component {
               defaultMessage="[CAUTION! NO DATA HAS BEEN SAVED. THIS IS NOT A REAL APPLICATION FOR SCHOOL MEAL BENEFITS. THIS IS A MODEL APPLICATION DEVELOPED BY USDA TO DEMONSTRATE THE POTENTIAL FUNCTIONALITY OF A SCHOOL DISTRICT'S APPLICATION. CONTACT YOUR CHILD'S SCHOOL TO FIND OUT WHERE YOU CAN ACCESS THEIR APPLICATION FOR SCHOOL MEAL BENEFITS.]"
         />
         </p>
+        <Button onClick={this.downloadData}>
+          <FormattedMessage
+              id="app.slides.thankYou.download"
+              description="Button to download data"
+              defaultMessage="Download Data"
+          />
+        </Button>
       </Slide>
     )
   }
