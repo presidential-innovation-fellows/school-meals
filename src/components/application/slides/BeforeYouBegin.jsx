d5588f0a93bbeb5e3dbe4438495ea259e6175c22
Ambit Developer <ambit.developer@gmail.com>
2016-11-22 14:49:27 -0500

    Tweak CSS

--- src/components/application/slides/BeforeYouBegin.jsx
+++ src/components/application/slides/BeforeYouBegin.jsx
@@ -146,10 +146,7 @@ class BeforeYouBegin extends React.Component {
               description="Click the question mark icons"
               defaultMessage="If you have questions at any point during the application, click the question mark icon {glyphIcon} to get help with the current section."
               values={{
-                glyphIcon: 
-                          <span className="nav-reference-glyph">
-                          <Glyphicon glyph="question-sign" />
-                          </span>
+                glyphIcon: <Glyphicon glyph="question-sign" className="help-icon" />
               }}
           />
         </p>
