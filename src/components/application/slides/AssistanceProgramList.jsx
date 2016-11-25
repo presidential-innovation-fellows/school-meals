60c0739cd67af90317731a77d0cada1e4ec66b1e
Ambit Developer <ambit.developer@gmail.com>
2016-11-25 16:38:34 -0600

    Create structure for responding to assistance program changes

--- src/components/application/slides/AssistanceProgramList.jsx
+++ src/components/application/slides/AssistanceProgramList.jsx
@@ -6,13 +6,38 @@ import { ApplicationData } from '../../../stores/ApplicationData'
 
 @observer
 class AssistanceProgramList extends Component {
+  constructor (props) {
+    super(props)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(fieldName, value, program) {
+    program[fieldName] = value
+
+    if (value) {
+      // clear FHMR status
+      // TODO
+
+      // clear adults other than attestor
+      // TODO
+
+      // clear other children
+      // TODO
+
+      // clear SSN
+      // TODO
+    }
+  }
+
   render() {
     const { assistancePrograms } = this.props.applicationData
 
     return(
       <Checkboxes legend="Assistance programs">
         {assistancePrograms.map(program =>
-          <AssistanceProgram program={program} key={program.id} />
+          <AssistanceProgram program={program}
+                             onChange={this.onChange}
+                             key={program.id} />
          )}
       </Checkboxes>
     )
