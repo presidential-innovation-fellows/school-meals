presidential-innovation-fellows/school-meals

commit 24eee878f8febaddbdfd71eaab11afa9a8f2a67a

Change summary Adults link to attestation if no household

src/components/application/slides/SummaryLabelContent.jsx
@@ -11,7 +11,7 @@ class SummaryLabelContent extends Component {
       <span>
         {this.props.id
          ?
-         <span>
+         <span key={this.props.id}>
            <Link id={this.props.id}>{this.props.children}</Link>
            {' '}
            <Link id={this.props.id}>
@@ -19,7 +19,7 @@ class SummaryLabelContent extends Component {
            </Link>
          </span>
          :
-         this.props.children
+         <span>this.props.children</span>
         }
       </span>
     )

src/components/application/slides/Summary.jsx
@@ -1,6 +1,7 @@
 ﻿import React, { Component, PropTypes, responsive, bordered } from 'react'
 import Slide from '../Slide'
 import SerialList from '../SerialList'
+import SummaryAdults from './SummaryAdults'
 import SummaryLabel from './SummaryLabel'
 import SummaryPersonCollection from './SummaryPersonCollection'
 import Checkbox from '../Checkbox'
@@ -21,14 +22,10 @@ class Summary extends Component {
 
   render() {
     const { applicationData } = this.props
-    const { adults,
-            contact,
+    const { contact,
             otherChildren,
             students } = applicationData
     const assistancePrograms = applicationData.assistancePrograms.applicable
-
-    // don't show link to Adults slide if we're not collecting household income
-    const adultsId = (applicationData.showHousehold || undefined) && 'adults'
 
     const headerText =
       <FormattedMessage
@@ -86,14 +83,7 @@ class Summary extends Component {
          </SummaryPersonCollection>
         }
 
-        <SummaryPersonCollection collection={adults} id={adultsId}
-                                 showDetails={applicationData.showHousehold}>
-          <FormattedMessage
-              id="app.slides.summary.adults"
-              description="Adults"
-              defaultMessage="Adults"
-          />
-        </SummaryPersonCollection>
+        <SummaryAdults applicationData={applicationData} />
 
         <div>
           <SummaryLabel id="assistance-programs">

src/components/application/slides/SummaryAdults.jsx
@@ -1,0 +1,33 @@
+import React, { Component, PropTypes } from 'react'
+import SummaryPersonCollection from './SummaryPersonCollection'
+import { observer } from 'mobx-react'
+import { FormattedMessage } from 'react-intl'
+
+@observer
+class SummaryAdults extends Component {
+  render() {
+    const { applicationData } = this.props
+    const id = applicationData.showHousehold ? 'adults' : 'attestation'
+
+    return (
+      <SummaryPersonCollection id={id}
+                               collection={applicationData.adults}
+                               showDetails={applicationData.showHousehold}>
+        <FormattedMessage
+            id="app.slides.summary.adults"
+            description="Adults"
+            defaultMessage="Adults"
+        />
+      </SummaryPersonCollection>
+    )
+  }
+}
+
+SummaryAdults.propTypes = {
+  applicationData: PropTypes.shape({
+    adults: PropTypes.object.isRequired,
+    showHousehold: PropTypes.bool.isRequired,
+  }).isRequired,
+}
+
+export default SummaryAdults

src/components/application/slides/SummaryPersonCollection.jsx
@@ -15,7 +15,8 @@ class SummaryPersonCollection extends Component {
           <ul>
             {
               collection.map(person => {
-                return <SummaryPerson person={person} key={person.id}
+                return <SummaryPerson person={person}
+                                      key={person.id}
                                       showDetails={showDetails} />
               })
             }

commit 99c6efdc6d87c49c4249de69955ea7d787c04cbe

Fix bug on SUmmary screen
Fixes #108
Introduced in 24eee878

src/components/application/slides/SummaryLabelContent.jsx
@@ -15,7 +15,7 @@ class SummaryLabelContent extends Component {
            <EditLink id={this.props.id}>{this.props.children}</EditLink>
          </span>
          :
-         <span>this.props.children</span>
+         <span>{this.props.children}</span>
         }
       </span>
     )