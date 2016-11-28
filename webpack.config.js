ff609afa09adc7a069bf1a538240cc768a1df1ff
Ambit Developer <ambit.developer@gmail.com>
2016-11-27 19:06:50 -0500

    Add linter to webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     'webpack-dev-server/client?http://localhost:3000',
     './src/index'
   ],
+  eslint: {
+    configFile: '.eslintrc.json'
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -19,10 +22,16 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      { test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      { enforce: 'pre',
+        test: /\.jsx?$/,
+        loaders: ['eslint-loader'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
   }
 };
